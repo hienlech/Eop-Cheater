7e0273ab0457c7f80a76068ed84abb7671418ac9
Hiển <hoilamgi7999@gmail.com>
2019-02-15 21:53:51 +0700

    Update 4.0

--- CheatEOP/ReadMe.txt
+++ CheatEOP/ReadMe.txt
@@ -1,10 +1,20 @@
-﻿Extention giúp tự động làm bài tiếng anh online(not unitest) trên trang eop.edu.vn
+﻿Extention giúp tự động làm bài tiếng anh online(not unitest) trên trang eop.edu.vn Update version 4.0
 Cách sử dụng :
    Nếu là bài bấm để nghe từ vựng thì chọn nút Listen Vocabulary
-   Nếu là bài chọn một trong các từ thì chọn nút Select Vocabulary
+   Nếu là bài chọn một trong các từ sau khi nghe thì chọn nút Select Vocabulary
 
-   Nếu là các bài phải điền từ thì chọn lần lượt Input , Copy và Parse (Bấm từ từ để nó còn load)
-   Nếu là các bài chọn câu hoặc từ bằng dấu tích thì chọn lần lượt Chọn đáp án , Copy và Parse trong phần chọn từ
+   Còn các bài còn lại có thể treo và để tool tự động làm (Bấm vài bài đầu rồi treo :))
+   Mỗi bài trong auto sẽ random thời gian làm từ 5 đến 10 phút 
+   
+ Cách bật auto : Vào trang có bài cần làm bấm F12 và vào màn hình console để xem hoạt động của tool 
+                 Sau đó ấn enable auto và treo tab đó thôi , tool sẽ tự động làm đến hết bài 
+
+   Lưu ý : Khi gặp lỗi có thể nhấn nút Stop và Bật lại 
+          - Nếu mạng chậm có thể gây ra lỗi , thời gian mặc định khi chuyển bài và tiếp tục làm là 15s	
+          
+
+ 
+
 P/S: Nếu bạn ngu và không muốn trượt tiếng anh thì đừng nên dùng extention này :v
 
 Developed by Hiển Lếch
--- CheatEOP/cheat.js
+++ CheatEOP/cheat.js
@@ -1,45 +1,97 @@
 //Code chạy trong cửa sổ chính trang web
-var InputAnswer = `var i = document.getElementsByTagName("input");
-    var submit = document.getElementById("dSubmit");
-    var viewA = document.getElementById("dViewAnswer");
-    var answer = [];
+var answer = `
+var count = 0;
+console.log("Start");
+var scrp = document.createElement("script");
+        scrp.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js");
+        var head = document.getElementsByTagName("head");
+        head[0].appendChild(scrp);//thêm thư viện jQuery
 
-    function Input() {
+function DoShit()
+{
+            //Lấy các phần tử để thao tác
+            var submit = document.getElementById("dSubmit");
+            var viewA = document.getElementById("dViewAnswer");
+            var body =document.querySelector("#mbody");
+            var c = document.querySelector(".dquestion");
+            var d;
 
-        for (var j = 0; j < i.length; j++) {
-            if (i[j].className === "danw dinline") {
-                i[j].value = "Answer";
+        //Nhập đáp án
+        function Input() {  
+        //     var Label=document.getElementsByTagName("label");
+        //     for(let i=0;i<Label.length;i++)
+        //     {
+        //         Label[i].click();
+        //     }
+        //    submit.click();
+            viewA.click(); // click để hiện đáp án
+        }
+        function Copy() { 
+            
+                d = Object.assign(c,d);
+                body.removeChild(c); //xóa element sau khi có đáp án  
+            
+
+        }
+
+        //trở về trang trả lời và append lại phần tử có đáp án
+        function Parse() {
+
+            viewA.click();
+            body.appendChild(d);
+            submit.click();
+            count++;
+            console.log("Đã xong " + count + " Bài");
+            
+            
+        }
+        //Lấy thời gian làm bài ngẫu nhiên từ 3 đến 5 phút
+        function getRandomTime() {
+           var x = Math.floor(Math.random() * (300000-150000) + 150000);
+          //var x = Math.floor((Math.random() * 20000) + 10000);
+
+            console.log(x);
+            return x;
+        }
+        //Chờ cho các button hiện lên để thao tác
+        function wait(ms) {
+            return new Promise(r => setTimeout(r, ms))  
+        }
+        async function Answer()
+        {
+            
+            try {
+                await Input();
+                await wait(1000);
+                await Copy();
+                var k = getRandomTime();
+                console.log("thời gian delay là " + k/60000+" phút");
+                console.log("Đang chờ...");
+                await wait(k);
+                await Parse(); 
+                console.log("Đang chờ chuyển trang , nếu mạng chậm gây lỗi thì refresh và bật tool lại..."); 
+                await wait(15000);
+            } catch (error) {
+                submit.click();
+                await wait(15000);
             }
-
+                
+                DoShit();
+        
+        
+            
         }
-        Click(submit);
-        Click(viewA);
+        Answer();
+        
+        
+}
+alert("Đã bật auto , vui lòng không bấm auto 2 lần để tránh xảy ra lỗi ")
+document.getElementsByTagName("body")[0].style.backgroundColor="#e74c3c";
+DoShit();
 
 
-    }
 
-    function Copy() {
-        answer = [];
-        for (var j = 0; j < i.length; j++) {
-            if (i[j].className === "danw dinline") {
-                answer.push(i[j].value);
-
-            }
-        }
-        Click(viewA);
-
-    }
-    function Click(a) {
-        let event = new Event("click");
-        a.dispatchEvent(event);
-    }
-
-    function Parse() {
-        for (var j = 0; j < answer.length; j++) {
-            i[j].value = answer[j];
-        }
-        Click(submit);
-    }`;
+`;
 
 
 
@@ -70,6 +122,9 @@ var selectVocabulary = `
                 parent[i].getElementsByTagName("span")[0].click();
             }
         }`;
+var stopEx =`
+    location.reload();
+`
 //Chạy code script trong tab hiện hành
 function ScriptExecute(codeString) {
 
@@ -87,61 +142,86 @@ function selectvoca(e) {
     ScriptExecute(selectVocabulary);
 }
 
-function input(e) {
-    ScriptExecute(InputAnswer + "Input();")
+function dostaff(e) {
+    ScriptExecute(answer)
 }
 
-function copy(e) {
-    ScriptExecute("Copy();");
-}
-
-function parse(e) {
-    ScriptExecute("Parse();");
+function stopDo(e) {
+    ScriptExecute(stopEx)
 }
 
 
 
-var select = `var submit = document.getElementById("dSubmit");
-var viewA = document.getElementById("dViewAnswer");
-var Label=document.getElementsByTagName("label");
-for(let i=0;i<Label.length;i++)
-{
-	Label[i].click();
-}
-submit.click();
-viewA.click();`;
-var cut = `var body =document.querySelector("#mbody");
-var c = document.querySelector(".dquestion");
-var d;
-d = Object.assign(c,d);
-body.removeChild(c);`;
-var parseCut = `viewA.click();
-body.appendChild(d);
-submit.click();`;
 
-function Select(e) {
-    ScriptExecute(select);
-}
 
-function Cut(e) {
-    ScriptExecute(cut);
-}
 
-function ParseCut(e) {
-    ScriptExecute(parseCut);
-}
 
 document.addEventListener('DOMContentLoaded', function () {
     var SelectVocabulary = document.getElementById("selectVocabulary");
     var Vocabulary = document.getElementById("vocabulary");
     SelectVocabulary.addEventListener('click', selectvoca);
     Vocabulary.addEventListener('click', voca);
-    document.getElementById("input").addEventListener('click', input);
-    document.getElementById("copy").addEventListener('click', copy);
-    document.getElementById("parse").addEventListener('click', parse);
-    document.getElementById("select").addEventListener('click', Select);
-    document.getElementById("cut").addEventListener('click', Cut);
-    document.getElementById("parseCut").addEventListener('click', ParseCut);
+    document.getElementById("dostaff").addEventListener('click', dostaff);
+    document.getElementById("stop").addEventListener('click', stopDo);
+
+    
 
 
 })
+
+
+// //test promise
+
+
+// var Answer = new Promise(function(resolve,reject){
+//         for (var j = 0; j < i.length; j++) {
+//         if (i[j].className === "danw dinline") {
+//             i[j].value = "Answer";
+//         }
+
+//     }
+//     submit.click();
+//     viewA.click();
+//     resolve("OK");
+// })
+// Answer.then(function(fullfilled){
+//     alert(fullfilled);
+//     answer = [];
+//         for (var j = 0; j < i.length; j++) {
+//             if (i[j].className === "danw dinline") {
+//                 answer.push(i[j].value);
+
+//             }
+//         }
+//         viewA.click();
+
+// }).then(
+//     function(fullfilled)
+//     {
+//         alert(fullfilled);
+//         for (var j = 0; j < answer.length; j++) {
+//             i[j].value = answer[j];
+//         }
+//         submit.click();
+//     }
+// ).catch(function(e)
+// {
+//     alert(e.message);
+// })
+
+// Dùng Async , await đơn giản hơn
+
+
+
+
+
+//copy element chứa đáp án
+
+
+
+
+
+// $(document).ajaxSuccess(function() {
+//     console.log("Tải xong")
+//   }); éo hiểu sao không hoạt động
+//nên khi mạng chậm có thể sẽ gặp lỗi
